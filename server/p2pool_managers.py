bb89d65622dc41349d39dfd74b29f40cc1f0df88
nate <nate.mcdonald@att.net>
2025-08-25 23:06:28 -0500

    made small changes

--- server/p2pool_managers.py
+++ server/p2pool_managers.py
@@ -1218,7 +1218,10 @@ class PythonRouterManager:
         iface_short = inbound_iface.split('_')[-1]
         ip_layer = packet.getlayer(IP) or packet.getlayer(IPv6)
         dst_ip = ip_layer.dst
-
+        if not packet.haslayer(Ether):
+            self.router_logger.log_message(
+                f"[Router] ❌ Dropping packet for {dst_ip} that has no Ether layer.")
+            return
         if not ip_layer:
             self.router_logger.log_message(f"[Router] ❗ No IP layer found in packet. Dropping.")
             return
